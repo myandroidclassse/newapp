b1616e38512ab2284684bfdcd4b640e3d47d6753
wjqaea <wjqaea@163.com>
2019-07-02 19:08:03 +0800

    Dao Class
    Dao class has been creat.

---
+++ Bookkeeping/app/src/main/java/com/app/entify/AimEntify.java
@@ -0,0 +1,42 @@
+package com.app.entify;
+
+import java.util.Date;
+
+public class AimEntify {
+    int ID;
+    String aim;
+    int money;
+    Date date;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getAim() {
+        return aim;
+    }
+
+    public void setAim(String aim) {
+        this.aim = aim;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
