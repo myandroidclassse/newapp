09589f6a1493a4958b667090bdd004326022d303
wjqaea <wjqaea@163.com>
2019-07-02 23:19:46 +0800

    UpData Dao Class interface
    All interface  has been created now time.

--- Bookkeeping/app/src/main/java/com/app/bookkeeping/MyDetail.java
+++ Bookkeeping/app/src/main/java/com/app/bookkeeping/MyDetail.java
@@ -7,5 +7,7 @@ class MyDetail extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bill);
+
     }
 }
--- Bookkeeping/app/src/main/java/com/app/bookkeeping/NewAsset.java
+++ Bookkeeping/app/src/main/java/com/app/bookkeeping/NewAsset.java
@@ -7,6 +7,7 @@ class NewAsset extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_account);
 
     }
 }
