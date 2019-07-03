6a18d018830b922bbd2c45b2736f349254b1a4e7
miTang <18275338890@163.com>
2019-07-03 17:58:41 +0800

    updata db

--- Bookkeeping/app/src/main/java/com/app/dao/MyDataBase.java
+++ Bookkeeping/app/src/main/java/com/app/dao/MyDataBase.java
@@ -18,9 +18,20 @@ public class MyDataBase {
             +"_id INTEGER primary key autoincrement," + "_name TEXT,"
             +"__type ";
 
+    private Context context;
     private DatabaseHelper dbHelper;
     private SQLiteDatabase mSqliteDatabase = null;
 
+    //TODO 打开数据库
+    public void open(){
+        dbHelper = new DatabaseHelper(context);
+        mSqliteDatabase = dbHelper.getWritableDatabase();
+    }
+
+    //TODO 关闭数据库
+    public void close(){
+        dbHelper.close();
+    }
 
 
     //TODO 获取assets表的所有记录,以id降序排序
@@ -60,10 +71,25 @@ public class MyDataBase {
         Cursor c =null;
         return c;
     }
+
+    public MyDataBase(Context context){
+        this.context = context;
+    }
+
    public static class DatabaseHelper extends SQLiteOpenHelper{
 
        public DatabaseHelper(Context context){
            super(context,DATABASE_NAME,null,DATABASE_VERSION);
+       }
+
+       @Override
+       public void onCreate(SQLiteDatabase db){
+           //创建表
+
+       }
+
+       @Override
+       public void onUpgrade(SQLiteDatabase db,int oldVersion, int newVersion){
        }
    }
 }
