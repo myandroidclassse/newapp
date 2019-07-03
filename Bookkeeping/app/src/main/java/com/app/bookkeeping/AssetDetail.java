17bc0120dfc72313cd38946b32e89f8dd86b193c
wjqaea <wjqaea@163.com>
2019-07-03 17:42:14 +0800

    new activity and dao

---
+++ Bookkeeping/app/src/main/java/com/app/bookkeeping/AssetDetail.java
@@ -0,0 +1,25 @@
+package com.app.bookkeeping;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AssetDetail extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asset_detail);
+
+
+
+    }
+
+    public void OnClickList(){
+        Intent intent = new Intent(AssetDetail.this,MyDetail.class);
+
+
+        startActivity(intent);
+
+        
+    }
+}
